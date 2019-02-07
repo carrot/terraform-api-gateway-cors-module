9b93e1abb808d97340cb1e0c5ea4d2ca8ffd8ec3
Carlos Garcia Arano <carlos.garciaarano@zooplus.com>
2019-02-07 08:59:58 +0100

    Format applied

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_api_gateway_method" "ResourceOptions" {
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
-  http_method = "OPTIONS"
+  rest_api_id   = "${var.rest_api_id}"
+  resource_id   = "${var.resource_id}"
+  http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
@@ -9,8 +9,9 @@ resource "aws_api_gateway_integration" "ResourceOptionsIntegration" {
   rest_api_id = "${var.rest_api_id}"
   resource_id = "${var.resource_id}"
   http_method = "${aws_api_gateway_method.ResourceOptions.http_method}"
-  type = "MOCK"
-  request_templates = { 
+  type        = "MOCK"
+
+  request_templates = {
     "application/json" = <<PARAMS
 { "statusCode": 200 }
 PARAMS
@@ -18,28 +19,33 @@ PARAMS
 }
 
 resource "aws_api_gateway_integration_response" "ResourceOptionsIntegrationResponse" {
-  depends_on = ["aws_api_gateway_integration.ResourceOptionsIntegration"]
+  depends_on  = ["aws_api_gateway_integration.ResourceOptionsIntegration"]
   rest_api_id = "${var.rest_api_id}"
   resource_id = "${var.resource_id}"
   http_method = "${aws_api_gateway_method.ResourceOptions.http_method}"
   status_code = "200"
+
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'",
-    "method.response.header.Access-Control-Allow-Origin" = "'${var.allowed_origin}'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'${var.allowed_origin}'"
   }
 }
 
 resource "aws_api_gateway_method_response" "ResourceOptions200" {
-  depends_on = ["aws_api_gateway_method.ResourceOptions"]
+  depends_on  = ["aws_api_gateway_method.ResourceOptions"]
   rest_api_id = "${var.rest_api_id}"
   resource_id = "${var.resource_id}"
   http_method = "OPTIONS"
   status_code = "200"
-  response_models = { "application/json" = "Empty" }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
-variable "resource_id"{}
+variable "resource_id" {}
 variable "rest_api_id" {}
 variable "allowed_origin" {}
