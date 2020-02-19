d76e120bec6fe14052569051fc77c14c0245f654
Jean Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2020-02-19 06:22:28 +0000

    upgrade to TF 0.12

--- main.tf
+++ main.tf
@@ -1,45 +1,49 @@
 resource "aws_api_gateway_method" "ResourceOptions" {
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
-  http_method = "OPTIONS"
+  rest_api_id   = var.rest_api_id
+  resource_id   = var.resource_id
+  http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "ResourceOptionsIntegration" {
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
-  http_method = "${aws_api_gateway_method.ResourceOptions.http_method}"
-  type = "MOCK"
-  request_templates = { 
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.ResourceOptions.http_method
+  type        = "MOCK"
+  request_templates = {
     "application/json" = <<PARAMS
 { "statusCode": 200 }
 PARAMS
+
   }
 }
 
 resource "aws_api_gateway_integration_response" "ResourceOptionsIntegrationResponse" {
-  depends_on = ["aws_api_gateway_integration.ResourceOptionsIntegration"]
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
-  http_method = "${aws_api_gateway_method.ResourceOptions.http_method}"
+  depends_on  = [aws_api_gateway_integration.ResourceOptionsIntegration]
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.ResourceOptions.http_method
   status_code = "200"
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'",
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 }
 
 resource "aws_api_gateway_method_response" "ResourceOptions200" {
-  depends_on = ["aws_api_gateway_method.ResourceOptions"]
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
+  depends_on  = [aws_api_gateway_method.ResourceOptions]
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
   http_method = "OPTIONS"
   status_code = "200"
-  response_models = { "application/json" = "Empty" }
+  response_models = {
+    "application/json" = "Empty"
+  }
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
   }
 }
+
